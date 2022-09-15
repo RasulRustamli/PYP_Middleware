1963df0d45368f8493d387aefd299d29ff3327e4
RasulRustamli <rasular@code.edu.az>
2022-09-16 02:29:29 +0400

    Add project files.

---
+++ PYP_Middleware/Extentions/ApplicationExtentions.cs
@@ -0,0 +1,12 @@
+﻿using PYP_Middleware.Middlewares;
+
+namespace PYP_Middleware.Extentions;
+
+public static class ApplicationExtentions
+{
+    public static IApplicationBuilder UseContent(this IApplicationBuilder app)
+    {
+       return app.UseMiddleware<CustomMiddleware>();
+        
+    }
+}
---
+++ PYP_Middleware/Middlewares/CustomMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace PYP_Middleware.Middlewares;
+
+public class CustomMiddleware
+{
+    private readonly IConfiguration _configuration;
+    private readonly RequestDelegate _next;
+
+    public CustomMiddleware(RequestDelegate next, IConfiguration configuration)
+    {
+        _next = next;
+        _configuration = configuration;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        //1.//_configuration.GetSection("CompanyInfo").GetChildren().ToList().ForEach(x =>
+        //{
+        //    context.Response.Headers.Add(x.Key, x.Value);
+        //});
+        //await _next.Invoke(context);
+       //2.
+        var dict = _configuration.GetSection("CompanyInfo").GetChildren().ToDictionary(x => x.Key, x => x.Value);
+
+        foreach (var key in dict)
+        {
+            context.Response.Headers.Add(key.Key, key.Value);
+        }
+        await _next.Invoke(context);
+
+    }
+}
